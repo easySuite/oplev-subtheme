310613f61f3de92e4c938086c8048cf689ced9ac
Mihaela Stavila <ms@inleadmedia.com>
2019-06-19 12:13:11 +0300

    EASYOPAC-1061 - Fix nodelist.

--- script/materials.js
+++ script/materials.js
@@ -40,6 +40,48 @@
       }
     });
 
+    //-------------------------------------------
+var sliderImg = $('.ding_nodelist.ding_nodelist-carousel .ding_nodelist-items.slick-slider .slick-list .slick-slide .event-image a img');
+
+function switchImgSliderStyles(element) {
+  var notMobile = ($(window).width() >=  768);
+  element.css({
+    'background-position': notMobile ? 'initial' : 'center',
+    'background-size': notMobile ? 'initial' : 'cover'
+  });
+}
+
+sliderImg.load(function() {
+  var img = $(this);
+  var div = $("<div />").css({
+    background: "url(" + img.attr("src") + ") no-repeat",
+    width: img.width() + "px",
+    height: img.height() + "px",
+    'max-width': img.naturalWidth ? img.naturalWidth : '100%',
+    'max-height': img.naturalHeight ? img.naturalHeight : '100%'
+  });
+
+  div.html(img.attr("alt"));
+  div.addClass("replaced-event-image");
+
+  img.replaceWith(div);
+  switchImgSliderStyles(div);
+});
+
+$(window).resize(function() {
+  var replacedDiv = $('.replaced-event-image');
+  var notMobile = ($(window).width() >=  768);
+
+  console.log(replacedDiv.css('max-width'), replacedDiv.css('max-height'));
+  replacedDiv.css({
+    width: notMobile ? replacedDiv.css('max-width') : replacedDiv.parent().width() + "px",
+    height: notMobile ? replacedDiv.css('max-height') : replacedDiv.parent().height() + "px"
+  });
+
+  switchImgSliderStyles(replacedDiv);
+});
+//---------------------------------------------
+
     var $header = $('.secondary-menu-wrapper');
     var $element = $('.secondary-menu li');
     var $body = $('.navbar-collapse .main-menu');
